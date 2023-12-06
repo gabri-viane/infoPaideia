5b2bb8836dec5dedbf53fc704e59c3a3e88f0d0d
gabri-viane <gabriviane@hotmail.it>
2023-12-06 10:55:30 +0100

    Inizio cilci

---
+++ SecondoAnno/Strutture/Lezione10/cicli_presentazione.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Il ciclo for serve per eseguire un numero definito di volte il codice
+    //in questo casio viene eseguito per 10 volte
+
+    //il for ha la seguente struttura:
+    // for ( dichiarazione variabili; condizione; operazioni)
+    //{
+    //
+    //}
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Stampo %d\n", i);
+    }
+
+    return 0;
+}
